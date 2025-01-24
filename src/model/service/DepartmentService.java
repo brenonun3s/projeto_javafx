43ef7d3ac0f335054c5126180b5a6a885fcd4cd5
Breno Nunes <morais.brenonunes@hotmail.com>
2025-01-24 16:57:02 -0300

    Adicionado tela de criar um novo departamento

--- src/model/service/DepartmentService.java
+++ src/model/service/DepartmentService.java
@@ -1,18 +1,18 @@
 package model.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll(){
-		List<Department> list = new ArrayList<Department>();
-		list.add(new Department(1, "Eletrônicos"));
-		list.add(new Department(2, "Móveis"));
+		return dao.findAll();
 
-		return list;
 	}
 
 }
